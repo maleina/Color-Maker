36c4787255f592c1df6058b95fc257009e7e7ab3
Maleina Bidek <maleina@ucla.edu>
2021-03-10 17:25:28 -0500

    Initial Commit

---
+++ Color Maker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color Maker
+//
+//  Created by Maleina Bidek on 3/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
