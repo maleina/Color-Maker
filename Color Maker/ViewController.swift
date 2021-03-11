491ee6321826c9f5bad951828889dd3c43cbd0f1
Maleina Bidek <maleina@ucla.edu>
2021-03-11 09:14:22 -0500

    feat: Wire up required outlets

--- Color Maker/ViewController.swift
+++ Color Maker/ViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var innerBox: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
